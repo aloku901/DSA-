0ff32f8542083033fde18055c7c89e0e023d2e01
Alok <aloku901@gmail.com>
2024-03-14 22:51:42 +0530

    Add All Update

--- MaxSubarraySum.java
+++ MaxSubarraySum.java
@@ -1,24 +1,24 @@
 public class MaxSubarraySum {
-    public static void Sumsubarr(int numbers[]) {
-        int curSum = 0;
-        int maxSum = Integer.MIN_VALUE;
+    // public static void Sumsubarr(int numbers[]) {
+    //     int curSum = 0;
+    //     int maxSum = Integer.MIN_VALUE;
 
-        for(int i=0; i<numbers.length; i++) {
-            int start =i;
-            for(int j=i; j<numbers.length; j++) {
-                int end = j;
-                curSum = 0;
-                for(int k=start; k<=end; k++) {
-                    curSum +=numbers[k];
-                }
-                System.out.println(curSum);
-                if(maxSum< curSum) {
-                    maxSum = curSum;
-                }
-            }
-        }
-        System.out.println("max sum = " + maxSum);
-    }
+    //     for(int i=0; i<numbers.length; i++) {
+    //         int start =i;
+    //         for(int j=i; j<numbers.length; j++) {
+    //             int end = j;
+    //             curSum = 0;
+    //             for(int k=start; k<=end; k++) {
+    //                 curSum +=numbers[k];
+    //             }
+    //             System.out.println(curSum);
+    //             if(maxSum< curSum) {
+    //                 maxSum = curSum;
+    //             }
+    //         }
+    //     }
+    //     System.out.println("max sum = " + maxSum);
+    // }
 
     public static void kadanes(int numbers[]) {
         int ms = Integer.MIN_VALUE;
